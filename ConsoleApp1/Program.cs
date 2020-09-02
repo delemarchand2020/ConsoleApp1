dd506b0c67db961ff7e56144e89399f4afd9d657
lemadeni <lemadeni@POLEMADENI>
2020-09-02 13:54:36 -0400

    test operation binaire

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Salut Polo!");
+			int a = 0b0001;
+			int b = 0b0011;
+			int c;
+
+			c = a & b;
+			Console.WriteLine("c=" + c.ToString());
 		}
 	}
 }
